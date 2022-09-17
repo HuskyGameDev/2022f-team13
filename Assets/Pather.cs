ee6c2b895f3c33693d14103e276e89bc27ba76a7
klink135 <mjjorgensen17@gmail.com>
2022-09-17 11:14:56 -0400

    Updated Controls
    -Built an improved control system that makes it easier to control.
    -I will be looking to improve it further by making it properly physics based.

--- Assets/Pather.cs
+++ Assets/Pather.cs
@@ -9,7 +9,9 @@ public class Pather : MonoBehaviour
     public EndOfPathInstruction endOfPathInstruction;
     public Rigidbody2D rb;
     public float speed = 10f;
-    float distanceTravelled;
+    public float distanceTravelled;
+    float smooth = 5.0f;
+    bool Held = false;
     Vector2 v2;
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,7 @@ public class Pather : MonoBehaviour
             // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
             pathCreator.pathUpdated += OnPathChanged;
         }
+        Held = false;
     }
 
     // Update is called once per frame
@@ -28,15 +31,26 @@ public class Pather : MonoBehaviour
         {
             if (Input.GetMouseButton(0) && Vector2.Distance(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)) < 1)
             {
-                distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                //This is where we check if held
+                Held = true;
+
+                
             } else
             {
                 distanceTravelled += 0 * Time.deltaTime;
             }
-            
 
+            if (Held)
+            {
+                distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            }
+
+            if(!Input.GetMouseButton(0))
+            {
+                Held = false;
+            }
             transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
-            transform.rotation = Quaternion.Euler(pathCreator.path.GetDirectionAtDistance(distanceTravelled));
+            transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
         }
     }
 
