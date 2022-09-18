c444f78481662fd166e946f90c5981fdb96ce2fd
klink135 <mjjorgensen17@gmail.com>
2022-09-18 11:16:29 -0400

    Put the train on a control loop that stops it from teleporting.
    If you want to change how reactive the train is. Just change the smooth variable.

--- Assets/Pather.cs
+++ Assets/Pather.cs
@@ -10,11 +10,11 @@ public class Pather : MonoBehaviour
     public EndOfPathInstruction endOfPathInstruction;
     public Rigidbody2D rb;
     public float speed = 10f;
-    public float currentPosition;
+    public float distanceTravelled;
     public float totalDistanceTravelled;
     float newDistanceTravelled;
     float oldDistanceTravelled;
-    float smooth = 5.0f;
+    public float smooth = 5.0f;
     bool Held = false;
     public Text distanceText;
     Vector2 v2;
@@ -37,20 +37,26 @@ public class Pather : MonoBehaviour
     {
         if (pathCreator != null)
         {
+            //Check if the mouse is in the proper position to start moving the train when clicked
             if (Input.GetMouseButton(0) && Vector2.Distance(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)) < 1)
             {
-                //This is where we check if held
+                //If so, set the Held variable that will allow the mouse almost free movement while handling the train
                 Held = true;
 
                 
             } else
             {
-                currentPosition += 0 * Time.deltaTime;
+                //Otherwise, that train is moving nowhere
+                distanceTravelled += 0 * Time.deltaTime;
             }
 
             if (Held)
             {
-                currentPosition = pathCreator.path.GetClosestDistanceAlongPath(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                //This is where the trains actual movement is held. Currently it teleports to closest track position to mouse.
+                //The goal is a to build a basic p loop that will move the train to the same designated point at a reasonable speed.
+                //distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(Camera.main.ScreenToWorldPoint(Input.mousePosition))
+
+                distanceTravelled += Mathf.Clamp((pathCreator.path.GetClosestDistanceAlongPath(Camera.main.ScreenToWorldPoint(Input.mousePosition)) - distanceTravelled) * smooth, -smooth, smooth) * Time.deltaTime;
                 AdjustDistance();
             }
 
@@ -58,15 +64,19 @@ public class Pather : MonoBehaviour
             {
                 Held = false;
             }
-            transform.position = pathCreator.path.GetPointAtDistance(currentPosition, endOfPathInstruction);
-            transform.rotation = pathCreator.path.GetRotationAtDistance(currentPosition, endOfPathInstruction);
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+            transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+
+
+            
         }
     }
 
     void OnPathChanged()
     {
-        currentPosition = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
     }
+    
     void AdjustDistance()
     {
         //check to see if the new position of the train has changed from before
