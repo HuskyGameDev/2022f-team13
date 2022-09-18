b0504293dca3b065c0faaa1ba2dd1350ba57874f
ZientDeveero <jmpigeon@mtu.edu>
2022-09-17 21:35:31 -0400

    Updated the distance traveled UI
    The distance travelled now accounts for forward and backwards distance

--- Assets/Pather.cs
+++ Assets/Pather.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PathCreation;
+using UnityEngine.UI;
 
 public class Pather : MonoBehaviour
 {
@@ -9,13 +10,20 @@ public class Pather : MonoBehaviour
     public EndOfPathInstruction endOfPathInstruction;
     public Rigidbody2D rb;
     public float speed = 10f;
-    public float distanceTravelled;
+    public float currentPosition;
+    public float totalDistanceTravelled;
+    float newDistanceTravelled;
+    float oldDistanceTravelled;
     float smooth = 5.0f;
     bool Held = false;
+    public Text distanceText;
     Vector2 v2;
     // Start is called before the first frame update
     void Start()
     {
+        distanceText.text = "Distance Traveled: 0";
+        oldDistanceTravelled = 0;
+        totalDistanceTravelled = 0;
         if (pathCreator != null)
         {
             // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
@@ -37,25 +45,40 @@ public class Pather : MonoBehaviour
                 
             } else
             {
-                distanceTravelled += 0 * Time.deltaTime;
+                currentPosition += 0 * Time.deltaTime;
             }
 
             if (Held)
             {
-                distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                currentPosition = pathCreator.path.GetClosestDistanceAlongPath(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                AdjustDistance();
             }
 
             if(!Input.GetMouseButton(0))
             {
                 Held = false;
             }
-            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
-            transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+            transform.position = pathCreator.path.GetPointAtDistance(currentPosition, endOfPathInstruction);
+            transform.rotation = pathCreator.path.GetRotationAtDistance(currentPosition, endOfPathInstruction);
         }
     }
 
     void OnPathChanged()
     {
-        distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        currentPosition = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+    }
+    void AdjustDistance()
+    {
+        //check to see if the new position of the train has changed from before
+        newDistanceTravelled = Mathf.Round(pathCreator.path.GetClosestDistanceAlongPath(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        if(!(oldDistanceTravelled == newDistanceTravelled)){
+            //update the total distance travelled
+            
+            totalDistanceTravelled += Mathf.Abs(oldDistanceTravelled - newDistanceTravelled);
+            //make sure old distance travelled is updated
+            oldDistanceTravelled = newDistanceTravelled;
+        }
+        //display new total distance travelled
+        distanceText.text = "Distance Traveled: " + totalDistanceTravelled;
     }
 }
