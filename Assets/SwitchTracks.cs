810f116050c407a66356880ef8b693a9bedb63be
klink135 <mjjorgensen17@gmail.com>
2022-10-12 18:05:36 -0400

    Track Switching is a thing now

---
+++ Assets/SwitchTracks.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation.Examples;
+
+public class SwitchTracks : MonoBehaviour
+{
+    public GameObject upper;
+    public GameObject lower;
+    public GameObject entrance;
+    private PathGenerator enter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        upper = this.transform.parent.Find("Upper").gameObject;
+        lower = this.transform.parent.Find("Lower").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (entrance == null)
+        {
+            entrance = upper.GetComponent<PathGenerator>().path_s;
+            enter = entrance.GetComponent<PathGenerator>();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Switching Tracks\n");
+        
+        if(enter.path_f.name == "Upper")
+        {
+            enter.path_f = lower;
+        } else if (enter.path_f.name == "Lower")
+        {
+            enter.path_f = upper;
+        }
+        
+    }
+}
